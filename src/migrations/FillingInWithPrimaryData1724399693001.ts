e7b0c08c0ac87e1403c336881d3851ca136d3b3c
Home <danila.peshev@kodep.ru>
2024-09-03 13:43:23 +0300

    The filling of primary data in the tables of films and genres has been removed.

--- src/migrations/FillingInWithPrimaryData1724399693001.ts
+++ src/migrations/FillingInWithPrimaryData1724399693001.ts
@@ -10,28 +10,9 @@ export class FillingInWithPrimaryData1724399693001
             INSERT INTO "user" ("id", "email", "password")
             VALUES (1, 'ivan@mail.ru', 'ivan');
         `);
-
-    await queryRunner.query(`
-            INSERT INTO "favorite_movie" ("id", "movieId", "isWatched", "userId")
-            VALUES (1, 932086, false, 1), 
-                   (2, 1048241, false, 1);
-        `);
-
-    await queryRunner.query(`
-            INSERT INTO "favorite_genre" ("id", "genreId", "userId")
-            VALUES (1, 37, 1);
-        `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`
-            DELETE FROM "favorite_genre" WHERE "id" = 1;
-        `);
-
-    await queryRunner.query(`
-            DELETE FROM "favorite_movie" WHERE "id" IN (1, 2);
-        `);
-
     await queryRunner.query(`
             DELETE FROM "user" WHERE "id" = 1;
         `);
